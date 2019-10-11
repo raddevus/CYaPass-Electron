191553256330245917c2ac48371352912b8e2a47
raddevus <1radeutsch1@gmail.com>
2019-10-11 11:19:22 -0400

    updated install pkg ver to 1.0.3, added png (for icon creation) -- updated main.js window creation to reference the PNG file. JPG isn't used but kept for history.

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    icon: 'CYaPass.jpg',
+    icon: 'CYaPass.png',
     webPreferences: {
       nodeIntegration: true,
       preload: path.join(__dirname, 'preload.js')
