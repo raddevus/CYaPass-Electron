be84ecffedd1200e279c54b36af74ffb0b3abb87
Roger Deutsch <1radeutsch1@gmail.com>
2023-09-04 11:25:17 -0400

    Added setupIcon which sets the icon for the app and the installer on windows.

--- forge.config.js
+++ forge.config.js
@@ -7,7 +7,9 @@ module.exports = {
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        setupIcon: "build/icon.ico"
+      },
     },
     {
       name: '@electron-forge/maker-zip',
